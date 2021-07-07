d221a8e374a375d3ca2f88a40b2098bb87f7fb5d
hansenwuwu <eric248655665@gmail.com>
2021-07-07 13:43:42 +0800

    update mongo init

--- mongo/init-mongo.js
+++ mongo/init-mongo.js
@@ -2,14 +2,26 @@ db = new Mongo().getDB("urlshortener");
 
 db.createCollection('pools', { capped: false });
 
+function makeid(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+}
+
 let count = 0;
 while (count < 500) {
-    let url_id = "";
-    while (true) {
-        url_id = nanoid();
-        var r = await db.pools.find_one({ url_id: url_id });
-        if (r === null) break;
-    }
-    await db.pools.insert({ url_id: url_id });
+    // let url_id = "";
+    // while (true) {
+    //     url_id = nanoid();
+    //     var r = await db.pools.find_one({ url_id: url_id });
+    //     if (r === null) break;
+    // }
+    let url_id = makeid(6);
+    db.pools.insert({ url_id: url_id });
     count++;
 }
